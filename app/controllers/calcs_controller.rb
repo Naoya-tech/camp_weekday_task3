96a23e3d6a3d0ff01814cd464b89fa30bae6fa97
Naoya-tech <naoya.techwork@gmail.com>
2019-03-05 21:32:18 +0900

    課題4 first commit

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -3,6 +3,8 @@ class CalcsController < ApplicationController
   def show
     @number1 = params[:number1]
     @number2 = params[:number2]
+    quotient = @number1.to_i / @number2.to_i
+    remainder = @number1.to_i % @number2.to_i
 
     if params[:operator] == "addition"
         @operator = @number1.to_i + @number2.to_i
@@ -11,8 +13,9 @@ class CalcsController < ApplicationController
     elsif params[:operator] == "multiplication"
         @operator =  @number1.to_i * @number2.to_i
     elsif params[:operator] == "division"
-        @operator = @number1.to_i / @number2.to_i 
+        @operator = "#{quotient} .. #{remainder}"
     end
+
   end
 
 end
