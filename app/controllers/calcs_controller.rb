d547c4c27742f6dee5ba653051548d3768dadebf
Naoya-tech <naoya.techwork@gmail.com>
2019-03-08 01:18:14 +0900

    計算ロジックの簡略化

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,18 +1,29 @@
 class CalcsController < ApplicationController
 
   def show
-    @number1 = params[:number1]
-    @number2 = params[:number2]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    operator = params[:operator]
 
-    if params[:operator] == "addition"
-        @operator = @number1.to_i + @number2.to_i
-    elsif params[:operator] == "subtraction"
-        @operator = @number1.to_i - @number2.to_i
-    elsif params[:operator] == "multiplication"
-        @operator =  @number1.to_i * @number2.to_i
-    elsif params[:operator] == "division"
-        @operator = @number1.to_i / @number2.to_i 
-    end
+    @result = case operator
+
+              when "addition"
+              @number1 + @number2
+
+              when "subtraction"
+              @number1 - @number2
+
+              when "multiplication"
+              @number1 * @number2
+
+              when "division"
+                if @number2 == 0
+                  "ゼロでは計算できません"
+                else
+                  @number1 / @number2.to_f
+                end
+              end
+              
   end
 
 end
