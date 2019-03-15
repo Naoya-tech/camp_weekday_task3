af436faa5800cb02f57eb8996f3c601d8a4fd114
Naoya-tech <naoya.techwork@gmail.com>
2019-03-15 19:21:52 +0900

    変数を修正

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,26 +1,26 @@
 class CalcsController < ApplicationController
 
   def show
-    @num1 = params[:num1].to_i
-    @num2 = params[:num2].to_i
+    num1 = params[:num1].to_i
+    num2 = params[:num2].to_i
     operator = params[:operator]
 
     @result = case operator
 
               when "addition"
-              @num1 + @num2
+              num1 + num2
 
               when "subtraction"
-              @num1 - @num2
+              num1 - num2
 
               when "multiplication"
-              @num1 * @num2
+              num1 * num2
 
               when "division"
-                if @num2 == 0
+                if num2 == 0
                   "ゼロでは計算できません"
                 else
-                  @num1 / @num2.to_f
+                  num1 / num2.to_f
                 end
               # :operatorが正しくないときの処理を追加
               else
